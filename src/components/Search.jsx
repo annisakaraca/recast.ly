34b2e4986f5445a81e16721ac2e4ca479adf0620
annisakaraca <annisakaraca@gmail.com>
2018-02-01 19:01:47 -0800

    Implements live search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,17 @@
 var Search = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
+    <input className="form-control" type="text" onKeyUp={(e) => {
+      var userText = $('.form-control').val();
+      if (e.keyCode === 13) {
+        if (userText !== '') {
+          searchYouTube({key: YOUTUBE_API_KEY, q: userText, max: 5}, props.onSearch);
+          $('.form-control').val('');
+        }
+      } else {
+        clearTimeout(window.debounce);
+        window.debounce = setTimeout(() => searchYouTube({key: YOUTUBE_API_KEY, q: userText, max: 5}, props.onSearch), 500)
+      }
+    }}/>
     <button className="btn hidden-sm-down" onClick={() => {
       var userText = $('.form-control').val();
       searchYouTube({key: YOUTUBE_API_KEY, q: userText, max: 5}, props.onSearch);
@@ -14,3 +25,4 @@ var Search = (props) => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.Search = Search;
+window.debounce = null;
